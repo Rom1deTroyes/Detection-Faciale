321ae12bfa23d8176de042d67e7f5d9e29e2e231
Romain Heller <Rom1deTroyes@users.noreply.github.com>
2022-05-05 10:45:42 +0200

    +[Images] detection de visages sur une image
    Signed-off-by: Romain Heller <Rom1deTroyes@users.noreply.github.com>

---
+++ image_detect.py
@@ -0,0 +1,17 @@
+import cv2
+
+cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
+img_path = "./images/001.jpg"
+color = (255, 255, 255) #La couleur du carré qui entoure le visage détecté
+
+src = cv2.imread(img_path,0)
+gray = cv2.cvtColor(src,cv2.cv2.COLOR_BAYER_BG2GRAY)
+cascade = cv2.CascadeClassifier(cascade_path)
+rect = cascade.detectMultiScale(gray)
+if len(rect) > 0:
+    for x, y, w, h in rect:
+        cv2.rectangle(src, (x, y), (x+w, y+h), color)
+
+        cv2.imshow('detected', src)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
