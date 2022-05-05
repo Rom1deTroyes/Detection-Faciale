d87e5d385d409cd17ca00755513ebb1dd09152ea
Romain Heller <Rom1deTroyes@users.noreply.github.com>
2022-05-05 11:04:27 +0200

    +[Images] add compteur
    Signed-off-by: Romain Heller <Rom1deTroyes@users.noreply.github.com>

--- image_detect.py
+++ image_detect.py
@@ -2,18 +2,23 @@ import cv2
 
 cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
 img_path = "./images/001.jpg"
-# color = (255, 255, 255)  # La couleur du carré qui entoure le visage détecté
-color = (0, 0, 0)  # La couleur du carré qui entoure le visage détecté
+color_text = (255, 255, 255)  # La couleur du carré qui entoure le visage détecté
+color_back = (0, 0, 0)  # La couleur du carré qui entoure le visage détecté
 hauteur_bandeau = 20
 
 src = cv2.imread(img_path,0)
 gray = cv2.cvtColor(src,cv2.cv2.COLOR_BAYER_BG2GRAY)
 cascade = cv2.CascadeClassifier(cascade_path)
 rect = cascade.detectMultiScale(gray)
+
+nb_personnes = 0
+
 if len(rect) > 0:
     for x, y, w, h in rect:
-        cv2.rectangle(src, (x, y), (x+w, y+h), color)
-        cv2.rectangle(src, (x, y - hauteur_bandeau), (x + w, y), color, -1)
+        nb_personnes += 1
+        cv2.rectangle(src, (x, y), (x+w, y+h), color_back)
+        cv2.rectangle(src, (x, y - hauteur_bandeau), (x + w, y), color_back, -1)
+        cv2.putText(src, f'Personne {nb_personnes}', (x, y - int(hauteur_bandeau/3)), cv2.FONT_HERSHEY_SIMPLEX, .5, color_text)
 
         cv2.imshow('detected', src)
         cv2.waitKey(0)
