fd6e8b8a8d020700622b8efe21ad4e991e84a867
Romain Heller <Rom1deTroyes@users.noreply.github.com>
2022-05-05 10:55:34 +0200

    +[Images] add Bandeau Signed-off-by: Romain Heller <Rom1deTroyes@users.noreply.github.com>

--- image_detect.py
+++ image_detect.py
@@ -2,7 +2,9 @@ import cv2
 
 cascade_path =  "./cascades/haarcascade_frontalface_default.xml"
 img_path = "./images/001.jpg"
-color = (255, 255, 255) #La couleur du carré qui entoure le visage détecté
+# color = (255, 255, 255)  # La couleur du carré qui entoure le visage détecté
+color = (0, 0, 0)  # La couleur du carré qui entoure le visage détecté
+hauteur_bandeau = 20
 
 src = cv2.imread(img_path,0)
 gray = cv2.cvtColor(src,cv2.cv2.COLOR_BAYER_BG2GRAY)
@@ -11,6 +13,7 @@ rect = cascade.detectMultiScale(gray)
 if len(rect) > 0:
     for x, y, w, h in rect:
         cv2.rectangle(src, (x, y), (x+w, y+h), color)
+        cv2.rectangle(src, (x, y - hauteur_bandeau), (x + w, y), color, -1)
 
         cv2.imshow('detected', src)
         cv2.waitKey(0)
